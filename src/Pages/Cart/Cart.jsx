48bfa7b0e713dc67f14afcee0cce45470487f443
favour <omojolaobaloluwa@gmail.com>
2025-08-16 22:08:55 +0100

    done

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -5,11 +5,12 @@ import "./Cart.css";
 import { RiDeleteBinLine, RiSubtractFill } from "react-icons/ri";
 import { FaRegTrashAlt } from "react-icons/fa";
 import { AiOutlinePlus } from "react-icons/ai";
+import Footer from "../../components/Footer/Footer";
 const Cart = () => {
   return (
     <div>
-      <NavSm />
       <NavBar />
+      <NavSm />
       <div className="home-container mt-3">
         <div className="bread-crumb mt-3">
           <span>Home</span>
@@ -51,12 +52,133 @@ const Cart = () => {
                   <RiSubtractFill />
                 </div>
               </div>
-              <div>price</div>
+              <div className="Cart-price">₦5,000</div>
+            </div>
+            <div className="cart-item">
+              <div className="d-flex align-items-center gap-2">
+                <div>
+                  <img
+                    src="https://freshcart.codescandy.com/assets/images/products/product-img-1.jpg"
+                    alt=""
+                    className="cart-image"
+                  />
+                </div>
+                <div>
+                  <div className="cart-item-name">name</div>
+                  <div className="cart-weight">1 kg</div>
+                  <div className="remove-section">
+                    <div className="cart-remove-icon ">
+                      <FaRegTrashAlt size={14} />
+                    </div>
+                    <div className="text">Remove</div>
+                  </div>
+                </div>
+              </div>
+              <div className="cart-counter">
+                <div className="cart-counter-btn">
+                  <AiOutlinePlus />
+                </div>
+                <div className="cart-amount">0</div>
+                <div className="cart-counter-btn">
+                  <RiSubtractFill />
+                </div>
+              </div>
+              <div className="Cart-price">₦5,000</div>
+            </div>
+            <div className="cart-item">
+              <div className="d-flex align-items-center gap-2">
+                <div>
+                  <img
+                    src="https://freshcart.codescandy.com/assets/images/products/product-img-1.jpg"
+                    alt=""
+                    className="cart-image"
+                  />
+                </div>
+                <div>
+                  <div className="cart-item-name">name</div>
+                  <div className="cart-weight">1 kg</div>
+                  <div className="remove-section">
+                    <div className="cart-remove-icon ">
+                      <FaRegTrashAlt size={14} />
+                    </div>
+                    <div className="text">Remove</div>
+                  </div>
+                </div>
+              </div>
+              <div className="cart-counter">
+                <div className="cart-counter-btn">
+                  <AiOutlinePlus />
+                </div>
+                <div className="cart-amount">0</div>
+                <div className="cart-counter-btn">
+                  <RiSubtractFill />
+                </div>
+              </div>
+              <div className="Cart-price">₦5,000</div>
+            </div>
+            <div className="cart-item">
+              <div className="d-flex align-items-center gap-2">
+                <div>
+                  <img
+                    src="https://freshcart.codescandy.com/assets/images/products/product-img-1.jpg"
+                    alt=""
+                    className="cart-image"
+                  />
+                </div>
+                <div>
+                  <div className="cart-item-name">name</div>
+                  <div className="cart-weight">1 kg</div>
+                  <div className="remove-section">
+                    <div className="cart-remove-icon ">
+                      <FaRegTrashAlt size={14} />
+                    </div>
+                    <div className="text">Remove</div>
+                  </div>
+                </div>
+              </div>
+              <div className="cart-counter">
+                <div className="cart-counter-btn">
+                  <AiOutlinePlus />
+                </div>
+                <div className="cart-amount">0</div>
+                <div className="cart-counter-btn">
+                  <RiSubtractFill />
+                </div>
+              </div>
+              <div className="Cart-price">₦5,000</div>
             </div>
           </div>
-          <div className="cart-summary">ss</div>
+          <div className="cart-summary">
+            <div className="cart-head">Summary</div>
+            <div className="shipping-head">Estimated shipping</div>
+            <div className="shipping-content">
+              Enter your destination to get a shipping estimate
+            </div>
+            <div className="price">
+              <div>sub-total</div>
+              <div className="cart-sub-price">3,000</div>
+            </div>
+            <div className="price">
+              <div>Coupon</div>
+              <div className="cart-sub-price">0</div>
+            </div>
+            <div className="price">
+              <div>coupon-discount</div>
+              <div className="cart-sub-price">0%</div>
+            </div>
+            <div className="price">
+              <input type="text" className="coupon-input" />
+              <button className="coupon-button">Apply</button>
+            </div>
+            <div className="line"></div>
+            <div className="price">
+              <div>Total</div>
+              <div className="cart-sub-price">3,000</div>
+            </div>
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
